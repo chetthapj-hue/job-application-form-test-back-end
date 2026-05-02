d5455363ef26a5c5dc8079a28e32e66213ee4d78
Chettha.pj <chettha.pj@gmail.com>
2026-05-02 12:42:40 +0700

    deplot render

--- back-end/server.js
+++ back-end/server.js
@@ -8,8 +8,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+// Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
